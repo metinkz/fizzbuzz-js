53fd3aa4892f8c4ee73638e469e588924b9c3b3d
Metin Karagöz <metinkzv@gmail.com>
2023-09-17 13:47:58 +0300

    add condition for buzz

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 let inputNumber = parseInt(prompt("Please enter a finishing number: "))
 
-for(let i = 1; i <= inputNumber; i++) {
-  if(i % 3 === 0) {
+for (let i = 1; i <= inputNumber; i++) {
+  if (i % 3 === 0) {
     console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz")
   } else {
     console.log(i)
   }
